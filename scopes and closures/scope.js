a1b046d80c4cec79f060234034cec79c752213af
hsnandeeshgowda <hsnandeeshgowda@gmail.com>
2020-09-18 00:02:02 +0530

    Scopes

--- scopes and closures/scope.js
+++ scopes and closures/scope.js
@@ -1,1 +1,23 @@
+debugger;
+
 a = 10;
+var b = 20;
+
+function calc(z){
+    
+    b = 40;
+    d = 50;
+    c = 10;
+
+    function subCalc(){
+        var c = 20;
+        d = d * z;
+        return d;
+    }
+
+    return subCalc();
+
+    var c;
+}
+
+console.log(calc(2));
